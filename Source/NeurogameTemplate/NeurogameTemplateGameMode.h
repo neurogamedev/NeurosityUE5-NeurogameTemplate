e353b71e89f2073828a1754f579ece83ccf73b5d
Diego Saldivar <88777150+neuromodgames@users.noreply.github.com>
2021-10-04 00:52:07 +0200

    Changes First Packet

---
+++ Source/NeurogameTemplate/NeurogameTemplateGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeurogameTemplateGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANeurogameTemplateGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANeurogameTemplateGameMode();
+};
+
+
+
